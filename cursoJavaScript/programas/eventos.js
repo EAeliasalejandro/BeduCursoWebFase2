c07bc6cfc62683e339dfe8c11c600e659aecd1ed
Elias Alejandro <eaeliasalejandro@hotmail.com>
2021-01-04 15:35:36 -0600

    ejercicio de eventos

--- cursoJavaScript/programas/eventos.js
+++ cursoJavaScript/programas/eventos.js
@@ -1,1 +1,19 @@
 
+const body = document.querySelector('body');
+const container = document.querySelector('#container');
+const button = document.querySelector('#enviar');
+const resultadof = document.querySelector('#resultado')
+/*body.addEventListener('click', function (event) {
+    console.log('contenedor pulsado');
+});
+container.addEventListener('click', function (event) {
+    console.log('body pulsado');
+});*/
+button.addEventListener('click', function (event) {
+    const numeroUno =Number.parseInt(document.getElementById('noUno').value); 
+    const numeroDos =Number.parseInt(document.getElementById('noDos').value);
+    let resultado=numeroUno+numeroDos;
+    resultadof.textContent = resultado;
+});
+
+
